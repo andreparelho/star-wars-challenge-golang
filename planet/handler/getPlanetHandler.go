a9d6c162146c4df7310d8a282419966f46f722e4
Andre Parelho <parelhoneves.andre@gmail.com>
2024-11-28 20:48:38 -0300

    feat: created call to external api service

--- planet/handler/getPlanetHandler.go
+++ planet/handler/getPlanetHandler.go
@@ -1,15 +1,23 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/andreparelho/star-wars-challenge-golang/planet/model/request"
 	"github.com/andreparelho/star-wars-challenge-golang/planet/service"
 	"github.com/gin-gonic/gin"
 )
 
 func GetPlanetHandler(context *gin.Context) {
+	var createPlanetRequest = request.CreatePlanetRequest{}
 
-	response := service.GetPlanets()
+	if error := context.BindJSON(&createPlanetRequest); error != nil {
+		log.Fatal("bind json error")
+		return
+	}
+
+	response := service.GetPlanets(createPlanetRequest, context)
 
 	context.JSON(http.StatusOK, response)
 }
