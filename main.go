6e9085cd8f57cbf3a738755f50d2e9310680e40c
Andre Parelho <parelhoneves.andre@gmail.com>
2024-11-28 21:34:06 -0300

    refact: refactoring main class, remove not used method and test router

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/andreparelho/star-wars-challenge-golang/planet/handler"
-	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,9 +11,7 @@ func main() {
 	fmt.Println("Starting server... ")
 
 	router := gin.Default()
-	router.Use(gzip.Gzip(gzip.DefaultCompression))
 
-	router.GET("/ping", handler.Pong)
 	router.GET("/planets", handler.GetPlanetHandler)
 
 	router.Run(":8080")
