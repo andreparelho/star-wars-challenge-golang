8b643e8a959a7ffb83e29a608c46bba21c9d01d5
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-11 00:27:12 -0300

    refactor: refactoring handler

--- planet/handler/getPlanetHandler.go
+++ planet/handler/getPlanetHandler.go
@@ -3,12 +3,22 @@ package handler
 import (
 	"net/http"
 
+	"github.com/andreparelho/star-wars-challenge-golang/planet/common"
 	"github.com/andreparelho/star-wars-challenge-golang/planet/service"
 	"github.com/gin-gonic/gin"
 )
 
 func GetPlanetHandler(context *gin.Context) {
-	response := service.GetAllPlanets(context)
+	planetsService := &service.GetAllPlanetsService{
+		Client: &service.RealHTTPClient{},
+	}
+
+	response, error := planetsService.GetAllPlanets(context)
+	if error != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			common.ERROR_KEY: common.HANDLER_ERROR_MESSAGE,
+		})
+	}
 
 	context.JSON(http.StatusOK, response)
 }
