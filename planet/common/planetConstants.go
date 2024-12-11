b8f655a958ff16d0572861be879e9ae2a4d3f747
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-10 21:43:04 -0300

    feat: create new constants

--- planet/common/planetConstants.go
+++ planet/common/planetConstants.go
@@ -1,3 +1,7 @@
 package common
 
 const API_URL = "https://swapi.dev/api/planets/"
+const ERROR_KEY string = "error"
+const BIND_ERROR_MESSAGE string = "bind json error"
+const CALL_API_ERROR_MESSAGE string = "Error to call external API"
+const JSON_PROCESS_ERROR_MESSAGE string = "Error to processing json"
